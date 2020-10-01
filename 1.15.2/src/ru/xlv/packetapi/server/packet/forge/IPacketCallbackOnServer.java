9ab386ab1a91a096abad73611fa39274cea1473e
Yevgeniy <evgeniytarasov.ru@gmail.com>
2020-10-01 02:17:03 +0200

    removed excess files

--- 1.15.2/src/ru/xlv/packetapi/server/packet/forge/IPacketCallbackOnServer.java
+++
@@ -1,6 +0,0 @@
-package ru.xlv.packetapi.server.forge.packet.forge;
-
-import net.minecraft.entity.player.ServerPlayerEntity;
-
-public interface IPacketCallbackOnServer extends IPacketCallbackOnServerRaw<ServerPlayerEntity> {
-}
